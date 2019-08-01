be9edf61c95dd1f784c559716be96b888f3b5eb3
marcocastignoli <marco.castignoli@gmail.com>
2019-08-01 02:13:19 +0200

    testing webhooks

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -4,7 +4,7 @@
       <b-row>
         <b-col class="text-center pt-4">
           <h1>Inchiesta</h1>
-          <p>Marcum ipsum dolor sit amet, consectetur adipiscing elit. Aenean gravida sagittis porta. Curabitur dapibus tincidunt ipsum, non ultrices enim dapibus sed. In cursus massa erat, at ullamcorper tortor hendrerit vel. Vestibulum eu mattis ex, sed ullamcorper lacus. Pellentesque non orci accumsan, rhoncus risus vel, laoreet eros. Nullam ut augue eget turpis rhoncus dignissim vitae sed sapien. Donec dignissim tellus imperdiet tellus tempor gravida.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean gravida sagittis porta. Curabitur dapibus tincidunt ipsum, non ultrices enim dapibus sed. In cursus massa erat, at ullamcorper tortor hendrerit vel. Vestibulum eu mattis ex, sed ullamcorper lacus. Pellentesque non orci accumsan, rhoncus risus vel, laoreet eros. Nullam ut augue eget turpis rhoncus dignissim vitae sed sapien. Donec dignissim tellus imperdiet tellus tempor gravida.</p>
         </b-col>
       </b-row>
       <b-row class="pt-3">
